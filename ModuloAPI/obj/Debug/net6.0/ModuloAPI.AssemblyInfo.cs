a2fb404a7fa6fa0a35796c349974179b9abaf346
Anderson Silva <anderson.ssilva@outlook.com>
2024-04-19 19:56:32 -0300

    initial commit

--- ModuloAPI/obj/Debug/net6.0/ModuloAPI.AssemblyInfo.cs
+++ ModuloAPI/obj/Debug/net6.0/ModuloAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fd55ca63a71ff70e9bb2f04115aa582a65b6615")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
